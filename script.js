66872802e760f9e195257355beb884b71bce5777
DobreCostin21 <dobrecostindragos@gmail.com>
2021-07-17 18:31:45 +0300

    s

---
+++ script.js
@@ -0,0 +1,11 @@
+function playSound() {
+    let x = document.getElementById("audioSound")
+    x.volume = 0.1;
+    x.play();
+}
+
+function pauseSound() {
+    let x = document.getElementById("audioSound")
+    x.volume = 0.1;
+    x.pause();
+}
